0c824b3137250a2ab2f68e849b14d1c2f91716ad
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-03-31 12:30:19 -0300

    :rotating_light: use: using the function converter currency

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-function sayHello(name: string) {
-    console.log(`Hello, ${name}!`);
+import { converterMoeda } from './converter';
+
+async function main() {
+    try {
+        const valorConvertido = await converterMoeda(100, 'USD', 'EUR');
+        console.log(`100 USD é equivalente a ${valorConvertido} EUR`);
+    } catch (error: any) {
+        console.error(`Erro: ${error.message}`);
+    }
 }
 
-sayHello("World");
+main();
